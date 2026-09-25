977abd30a237e65a942331e1c588d5ed4fbd0604
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-09-24 21:02:43 -0300

    Rotas

--- section03_express_js/index.js
+++ section03_express_js/index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send('Lista de usuários')
+})
+
+app.get('/sobre', (req, res) => {
+    res.send('Sobre o projeto')
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 })
